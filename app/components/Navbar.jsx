739a87ec23d60707662cd7e8c39772ebc67f69f8
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-11-14 21:14:52 -0300

    styling fix

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,7 @@
 // Navbar.jsx
 export default function Navbar() {
     return (
-      <nav className="bg-slate-400 p-4 shadow-lg">
+      <nav className="bg-slate-800 p-4 shadow-lg">
         <div className="container mx-auto flex justify-between items-center">
           <h1 className="text-2xl font-bold text-pink-300">Guess<span className="text-white font-medium">The</span>Woof</h1>
           <ul className="flex space-x-4">
