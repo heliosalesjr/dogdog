2f399df0c678e7fe0a3d7cf13824e9b45cbe911f
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-05-30 21:10:51 -0300

    moved navbar to layout

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
-// page.jsx
-import Navbar from './components/Navbar';
+
 import HeroSection from './components/HeroSection';
 import AboutUs from './components/AboutUs';
 import Game from './components/Game';
@@ -8,7 +7,7 @@ import AboutTheProject from './components/AboutTheProject';
 export default function HomePage() {
   return (
     <div>
-      <Navbar />
+      
       <HeroSection />
       <Game /> 
       
