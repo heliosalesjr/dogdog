df5b0c6ee931880fbd1c27102f5c2129be3a2b3d
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-06-01 21:19:27 -0300

    footer updated

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer() {
         <div className="container mx-auto text-center my-8">
           <p className="text-white text-sm">&copy; {new Date().getFullYear()} <span className="font-black">Hélio Sales Jr.</span> All rights reserved.</p>
           <div className="flex justify-center space-x-4 mt-4">
-            <a href="#" className="text-white hover:text-orange-200">Privacy Policy</a>
-            <a href="#" className="text-white hover:text-orange-200">Terms of Service</a>
+            <a href="#" className="text-white hover:text-orange-200">Github</a>
+            <a href="#" className="text-white hover:text-orange-200">Linkedin</a>
           </div>
         </div>
       </footer>
--- app/page.js
+++ app/page.js
@@ -10,7 +10,6 @@ export default function HomePage() {
       
       <HeroSection />
       <Game /> 
-      
       <AboutTheProject />
       <AboutUs />
       <Footer />
